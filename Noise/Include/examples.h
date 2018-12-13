a10845c5770efc241eea973a52399731426827b8
Mathieu Gaillard <gaillard.mathieu.39@gmail.com>
2018-12-12 22:50:39 -0500

    Document the performance testing.

--- Noise/Include/examples.h
+++ Noise/Include/examples.h
@@ -27,6 +27,23 @@ void LichtenbergFigureImage(int width, int height, int seed, const std::string& 
 
 void EffectParametersImage(int width, int height, int seed, int resolution, double eps, double displacement, const std::string& filename);
 
+/**
+ * \brief Measure the time in ms taken to generate Lichtenberg figure.
+ * #!/bin/bash
+ * for i in {256..4..4}
+ *   do
+ *     echo "Num Threads $i"
+ *     export OMP_NUM_THREADS=$i
+ *     for j in {1..3}
+ *       do
+ *         ./Noise
+ *       done
+ *   done
+ * \param width Resolution in the width axis
+ * \param height Resolution in the height axis
+ * \param filename File in which the result is saved, ideal case: check it with a checksum.
+ * \return The time taken to generate the image in ms.
+ */
 double PerformanceTest(int width, int height, const std::string& filename);
 
 #endif // EXAMPLES_H
